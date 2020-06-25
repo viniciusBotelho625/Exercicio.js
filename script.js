b032c18e7d11b0ffb8216e1aee1d0df04b803202
viniciusBotelho625 <58275652+viniciusBotelho625@users.noreply.github.com>
2020-06-25 20:36:17 -0300

    Estilo

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
 function carregar() {
     var msg = window.document.getElementById('msg')
     var img = window.document.getElementById('foto')
+    var mensage = window.document.getElementById('mensage')
     var data = new Date()
-    var hora = data.getHours()
-    
+    //var hora = data.getHours()
+    var hora = 9
     msg.innerHTML = `Agora são ${hora} horas`
     if(hora >= 0 && hora < 12) {
-        img.src = 'bom-dia.jpg'
+        img.src = '../img/bom-dia.jpg'
+        mensage.innerHTML = 'Bom dia'
     } else if(hora >= 12 && hora < 18) {
-        img.src = 'boa-tarde.jpg'
+        img.src = '../img/boa-tarde.jpg'
+        mensage.innerHTML = 'Boa Tarde'
     } else {
-        img.src = 'boa-noite.jpg'
+        img.src = '../img/boa-noite.jpg'
+        mensage.innerHTML = 'Boa Noite'
     }
 }
